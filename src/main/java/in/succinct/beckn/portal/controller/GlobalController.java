ad0a466f500dd71aef626945d172e6a0c4d42892
Venky <venkatramanm@gmail.com>
2021-07-15 23:18:48 +0530

    Template for Ravi

--- src/main/java/in/succinct/beckn/portal/controller/GlobalController.java
+++ src/main/java/in/succinct/beckn/portal/controller/GlobalController.java
@@ -1,22 +1,20 @@
 package in.succinct.beckn.portal.controller;
 
-import com.venky.swf.controller.annotations.RequireLogin;
 import com.venky.swf.path.Path;
 import com.venky.swf.plugins.templates.controller.TemplatedController;
 import com.venky.swf.views.View;
 
-public class DashboardController extends TemplatedController {
-    public DashboardController(Path path) {
+public class GlobalController extends TemplatedController {
+    public GlobalController(Path path) {
         super(path);
     }
 
     @Override
     public String getTemplateDirectory() {
-        return super.getTemplateDirectory("dashboard");
+        return super.getTemplateDirectory("global");
     }
 
-    @RequireLogin
     public View index(){
-        return html("index",true);
+        return html("index");
     }
 }
